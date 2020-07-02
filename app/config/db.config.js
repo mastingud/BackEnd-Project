372cf591df5876b82dd1923b79fcb8d488cd3fc2
mastingud <63357619+mastingud@users.noreply.github.com>
2020-07-02 23:26:52 +0700

    update db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "dhio",
-    PASSWORD: "1234",
-    DB: "testdb2",
-    dialect: "mysql",
-    pool: {
-      max: 105,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b9ad173eb97efd",
+  PASSWORD: "2529ec80",
+  DB: "heroku_b85dba1e77c7a7b",
+  dialect: "mysql",
+  pool: {
+    max: 105,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
 
-  
-// CLEARDB_DATABASE_URL: mysql://b9ad173eb97efd:2529ec80@us-cdbr-east-05.cleardb.net/heroku_b85dba1e77c7a7b?reconnect=true
